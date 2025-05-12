f489df400f79b4d92e9edd1a3381947e5834cb7b
Forestrunner51 <airdisciple23@gmail.com>
2025-05-12 09:31:10 -0500

    aded movemenmt

--- Character/MC/player.gd
+++ Character/MC/player.gd
@@ -1,1 +1,10 @@
 extends CharacterBody2D
+
+@export var speed = 400
+@export var jumpspeed =-1000
+	
+		
+func _physics_process(delta):
+	var direction = Input.get_vector("left", "right", "up", "down")
+	velocity = direction * speed
+	move_and_slide()
